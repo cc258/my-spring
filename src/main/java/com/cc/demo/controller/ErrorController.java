59ce691033fe70dc1becd69159a7b2fccfefaa8b
Leo <chuang.cui@ericsson.com>
2021-04-28 17:52:10 +0800

    chore(Others): global exception test

--- src/main/java/com/cc/demo/controller/ErrorController.java
+++ src/main/java/com/cc/demo/controller/ErrorController.java
@@ -9,6 +9,14 @@ public class ErrorController {
     @GetMapping("/error500")
     public String Error500(){
         int err = 50/0;
-        return "abc";
+        return "error500";
+    }
+
+    @GetMapping("/error409")
+    public String Error409(Integer num){
+        if(num > 2) {
+            throw new ArithmeticException();
+        }
+        return "main";
     }
 }
--- src/main/java/com/cc/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cc/demo/exception/GlobalExceptionHandler.java
@@ -8,12 +8,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
+    // 第一种异常处理的方式, 返回model and view
     // ArithmeticException 数学运算错误
     // NullPointerException 空指针错误
     @ExceptionHandler({ArithmeticException.class, NullPointerException.class})
     public String handleArithException(Exception e){
 
         log.error("异常：{}", e);
-        return "/mm/main.html";
+
+        // return 视图地址 model and view
+        return "/mm/5xx.html";
     }
+
+
 }
