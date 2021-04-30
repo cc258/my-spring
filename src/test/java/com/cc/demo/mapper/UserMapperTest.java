ecd31053c2056b9d55a623367d268979625604e3
Leo <chuang.cui@ericsson.com>
2021-04-30 16:18:19 +0800

    chore(Others): import mybatis plus & junit 5

--- src/test/java/com/cc/demo/mapper/UserMapperTest.java
+++ src/test/java/com/cc/demo/mapper/UserMapperTest.java
@@ -8,9 +8,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @Slf4j
 @SpringBootTest
 class UserMapperTest {
+
+    // 前面的测试报错，后面将不再执行
 
     @Autowired
     UserMapper userMapper;
@@ -44,6 +48,43 @@ class UserMapperTest {
         System.out.println(("----- selectAll method test ------"));
         List<User> userList = userMapper.selectList(null);
         userList.forEach(System.out::println);
+    }
+
+    // 测试数值等
+    @Test
+    void testAssertEquals(){
+        int num = 2 + 3;
+        assertEquals(5, num, "数值不等于5");
+    }
+
+    // 测试同一个对象
+    @Test
+    void testAssertSame(){
+        Object o1 = new Object();
+        Object o2 = new Object();
+        assertSame(o1,o2,"对象的内存地址不一样");
+    }
+
+    // 测试数组
+    @Test
+    void testArray(){
+        Object o1 = new Object();
+        Object o2 = new Object();
+        assertArrayEquals(new int[]{1,2},new int[]{1,2},"数组内容不相等");
+    }
+
+    // 测试异常
+    @Test
+    void testException(){
+        assertThrows(ArithmeticException.class, ()->{int i = 10/0; }, "居然没异常，有问题");
+    }
+
+    // 测试快速失败
+    @Test
+    void testFail(){
+        if(1==2){
+            fail("主动测试失败");
+        }
     }
 
 }
